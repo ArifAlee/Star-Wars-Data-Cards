3f4121a6f81112723473975c5697a847480fde59
Arif Ali <Arifalee241@gmail.com>
2025-02-09 17:13:57 +0000

    Fixed CORS issue

--- app.js
+++ app.js
@@ -14,20 +14,45 @@ const homeworld = document.querySelector(".homeworld");
 let characters;
 
 // Get data from starwars api
-const getCharData = async function () {
-  const charData = [];
-  let requests = [];
-  for (let i = 1; i < 84; i++) {
-    if (i === 17) continue;
-    //no details found for #17, will be skipped
+// const getCharData = async function () {
+//   const charData = [];
+//   let requests = [];
+//   for (let i = 1; i < 84; i++) {
+//     if (i === 17) continue;
+//     //no details found for #17, will be skipped
       
-    requests.push(axios.get(`https://swapi.dev/api/people/${i}`)
-    .then(res => charData.push(res.data))
-    .catch(error => console.log(`Error fetching data for characters ${i}`, error)))
+//     requests.push(axios.get(`https://swapi.dev/api/people/${i}`)
+//     .then(res => charData.push(res.data))
+//     .catch(error => console.log(`Error fetching data for characters ${i}`, error)))
+//   }
+//   await Promise.all(requests)
+//   return charData;
+// };
+
+
+const getAllCharacters = async () => {
+  let characters = [];
+  let url = "https://swapi.dev/api/people/";
+
+  try {
+    while (url) {
+      const response = await axios.get(url);
+      characters = characters.concat(response.data.results);
+      url = response.data.next; // URL for the next page
+    }
+    return characters;
+  } catch (error) {
+    console.log("Error fetching characters:", error);
+    return [];
   }
-  await Promise.all(requests)
-  return charData;
 };
+
+// Usage
+getAllCharacters().then((characters) => {
+  console.log(characters); // Array of all characters
+});
+
+
 
 
 const getCharObj = (name) => {
@@ -57,6 +82,7 @@ const updateCharDetails = (name) => {
   }
 };
 
+
 // USER INPUT HANDLER
 const handleUserInput = () => {
   const userInput = input.value.trim()
@@ -67,19 +93,17 @@ const handleUserInput = () => {
 }
 
 // EVENT LISTENERS
+
 document.addEventListener("DOMContentLoaded", async() => {
   try {
-    characters = await getCharData();
+    characters = await getAllCharacters();
     console.log(characters)
   } catch(error) {
     console.log("Unable to get character data", error)
   }
 });
 
-
-
 submitBtn.addEventListener("click", handleUserInput)
-// Submit by pressing ENTER
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
    handleUserInput();
